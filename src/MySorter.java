089cc3a0c1ff8fbce5016b3cd12f567fec72fe1c
Shy <s.daniell05@outlook.com>
2026-02-23 12:49:20 -0500

    WORK FOR THE LOVE OF MY SANITY

---
+++ src/MySorter.java
@@ -0,0 +1,13 @@
+public class MySorter {
+
+    /*
+     * Returns the authors' names.
+     *  @return The names of the authors of this file.
+     */
+    public static String getAuthors() {
+        return "Shy Daniell and Elle Spivey";
+    }
+
+
+
+}
